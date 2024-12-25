a324f3d71707e71fa0e52dc3426a11f9256d837b
Benson607 <benson940718@gmail.com>
2024-12-26 00:41:58 +0800

    add signup

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("title_icon").addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+});
