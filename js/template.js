6c8c5fb56c9e4f03a40233b4d42bf2a9ce89a4ee
Benson607 <benson940718@gmail.com>
2024-12-27 07:08:26 +0800

    change to new left list

--- js/template.js
+++ js/template.js
@@ -18,47 +18,26 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://alus.us.kg/api/check_login", {
         method: "POST"
     }).then(response => response.json())
-    .then(data => {/*
-        if (data.msg == "logined") {
-            left_list.innerHTML = ''
-            +'<form action="/this_site/index.html">'
-            +    '<button type="submit" id="about_button" class="func_button">關於本網站</button>'
-            +'</form>'
-            +'<form action="/login.html">'
-            +    '<button type="submit" id="to_login_button" class="func_button">登入</button>'
-            +'</form>';
-        }
-        else {
-            left_list.innerHTML = ''
-            +'<form action="/this_site/index.html">'
-            +    '<button type="submit" id="about_button" class="func_button">關於本網站</button>'
-            +'</form>'
-            +'<form action="/signup.html">'
-            +    '<button type="submit" id="to_signup_button" class="func_button">註冊</button>'
-            +'</form>'
-            +'<form action="/login.html">'
-            +    '<button type="submit" id="to_login_button" class="func_button">登入</button>'
-            +'</form>';
-        }*/
+    .then(data => {
         if (data.msg == "logined") {
             left_list.innerHTML = ''
             + "<li>"
-            + "<a href='/this_site/index.html'>關於本網站</a>"
+            + "<a class='left_button' href='/this_site/index.html'>關於本網站</a>"
             + "</li>"
             + "<li>"
-            + "<a href='/logout.html'>登出</a>"
+            + "<a class='left_button' href='/logout.html'>登出</a>"
             + "</li>";
         }
         else {
             left_list.innerHTML = ''
             + "<li>"
-            + "<a href='/this_site/index.html'>關於本網站</a>"
+            + "<a class='left_button' href='/this_site/index.html'>關於本網站</a>"
             + "</li>"
             + "<li>"
-            + "<a href='/signup.html'>註冊</a>"
+            + "<a class='left_button' href='/signup.html'>註冊</a>"
             + "</li>"
             + "<li>"
-            + "<a href='/login.html'>登入</a>"
+            + "<a class='left_button' href='/login.html'>登入</a>"
             + "</li>";
         }
     })
