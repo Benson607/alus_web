011747ca20a7ec3bf43e0c44ed0b1c991e312b96
Benson607 <benson940718@gmail.com>
2024-12-27 17:21:33 +0800

    fix logout button load bug

--- js/main.js
+++ js/main.js
@@ -11,21 +11,24 @@ function cancel_logout() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("logout_button").addEventListener("click", () => {
-        var mask = document.createElement("div")
-        mask.id = "mask";
-        mask.className = "mask";
-        document.body.appendChild(mask);
-        var logout_dialog = document.createElement("div");
-        logout_dialog.id = "logout_dialog";
-        logout_dialog.className = "logout_div";
-        logout_dialog.innerHTML = ""
-        + "<h1>你確定要退出嗎?</h1>"
-        + "</br>"
-        + "<button id='yes'>確定</button>"
-        + "</br>"
-        + "<button id='no'>取消</button>";
-        document.body.appendChild(logout_dialog);
-        document.getElementById("yes").addEventListener("click", logout);
+    var logout_button = document.getElementById("logout_button");
+    logout_button.addEventListener("load", () => {
+        logout_button.addEventListener("click", () => {
+            var mask = document.createElement("div");
+            mask.id = "mask";
+            mask.className = "mask";
+            document.body.appendChild(mask);
+            var logout_dialog = document.createElement("div");
+            logout_dialog.id = "logout_dialog";
+            logout_dialog.className = "logout_div";
+            logout_dialog.innerHTML = ""
+            + "<h1>你確定要登出嗎?</h1>"
+            + "</br>"
+            + "<button id='yes'>確定</button>"
+            + "</br>"
+            + "<button id='no'>取消</button>";
+            document.body.appendChild(logout_dialog);
+            document.getElementById("yes").addEventListener("click", logout);
+        });
     });
 });
